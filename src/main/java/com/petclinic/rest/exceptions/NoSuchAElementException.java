422dcaba2baef891a60262a1119db4913cafa927
tableonthewall <seydaozdemir07@gmail.com>
2021-11-25 10:38:44 +0300

    25/11/2021 default constructor created

--- src/main/java/com/petclinic/rest/exceptions/NoSuchAElementException.java
+++ src/main/java/com/petclinic/rest/exceptions/NoSuchAElementException.java
@@ -1,6 +1,13 @@
 package com.petclinic.rest.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+//@ResponseStatus(HttpStatus.NOT_FOUND)
 public class NoSuchAElementException extends RuntimeException{
+    public NoSuchAElementException(){
+        super();
+    };
     public NoSuchAElementException(String message) {
         super(message);
     }
