92690e30c6e44f398b4ceb34ecd89186fa089fd9
tableonthewall <seydaozdemir07@gmail.com>
2021-11-25 10:39:09 +0300

    25/11/2021 OwnerControllerTest getOwnerByIdTestNotFound()

--- src/test/java/com/petclinic/rest/controller/OwnerControllerTest.java
+++ src/test/java/com/petclinic/rest/controller/OwnerControllerTest.java
@@ -1,30 +1,34 @@
 package com.petclinic.rest.controller;
 
 import com.petclinic.rest.dto.OwnerDto;
-import com.petclinic.rest.model.Owner;
+import com.petclinic.rest.exceptions.NoSuchAElementException;
+import com.petclinic.rest.exceptions.NoSuchElementAdvice;
 import com.petclinic.rest.service.OwnerService;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
-import org.springframework.test.web.servlet.MockMvcResultHandlersDsl;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.w3c.dom.stylesheets.LinkStyle;
-import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -47,6 +51,7 @@ class OwnerControllerTest {
 
         mockMvc= MockMvcBuilders
                 .standaloneSetup(ownerController)
+                .setControllerAdvice(new NoSuchElementAdvice())
                 .build();
     }
 
@@ -54,8 +59,11 @@ class OwnerControllerTest {
     void getAll() throws Exception {
         when(ownerService.findAll()).thenReturn(ownerDtos);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/owners"))
-                .andExpect(MockMvcResultMatchers.status().is(200));
+        mockMvc.perform(MockMvcRequestBuilders.get("/owners")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is(200))
+
+                .andExpect(jsonPath("$", Matchers.hasSize(2)));
         //List<OwnerDto> list=ownerController.getAll();
 
         //assertEquals(2,list.size());
@@ -65,8 +73,24 @@ class OwnerControllerTest {
     void find() throws Exception {
         when(ownerService.findById(anyLong())).thenReturn(new OwnerDto(1L,"seyda","özdemir","asdsa","asdasd","dasd",null));
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/owners/1"))
-                .andExpect(MockMvcResultMatchers.status().is(200));
+        mockMvc.perform(MockMvcRequestBuilders.get("/owners/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is(200))
+
+                .andExpect(jsonPath("$").exists());
+    }
+
+    @Test
+    public void getOwnerByIdTestNotFound() throws Exception {
+        //when(ownerMapper.toDTO(owner)).thenReturn(ownerDto);
+//        Assertions.assertThrows(NoSuchAElementException.class, () -> {
+//            service.findById(3L);
+//        });
+
+        when(ownerService.findById(anyLong())).thenThrow(NoSuchAElementException.class);
+
+        mockMvc.perform(get("/owners/1"))
+                .andExpect(status().isNotFound());
     }
 
     @Test
