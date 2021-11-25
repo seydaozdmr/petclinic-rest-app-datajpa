7bac46fd80c13f34dae6a8930231ff2a526e2909
tableonthewall <seydaozdemir07@gmail.com>
2021-11-25 22:34:21 +0300

    25/11/2021 fixed test bug

--- src/main/java/com/petclinic/rest/exceptions/NoSuchAElementException.java
+++ src/main/java/com/petclinic/rest/exceptions/NoSuchAElementException.java
@@ -3,7 +3,7 @@ package com.petclinic.rest.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-//@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class NoSuchAElementException extends RuntimeException{
     public NoSuchAElementException(){
         super();
